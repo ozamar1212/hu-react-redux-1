b0670f228ff7b5f390c7d9822fb1eb90732478a5
bk-software <barak.software.solutions@gmail.com>
2022-11-30 18:20:49 +0200

    add react with jsx example

---
+++ examples/01_basic-react_with_jsx/app.js
@@ -0,0 +1,7 @@
+function Hello() {
+  return <div>Hello to jsx</div>;
+}
+
+const rootNode = document.getElementById('root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(Hello));
