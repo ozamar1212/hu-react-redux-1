5c4a3a6eead6712aed5bea01e4583f7855197afb
bk-software <barak.software.solutions@gmail.com>
2022-11-30 20:14:01 +0200

    use params in component

--- react-apps/src/applications/basic-app/App.js
+++ react-apps/src/applications/basic-app/App.js
@@ -1,5 +1,17 @@
+function SayHello(props) {
+  console.log(props);
+
+  return <div style={{ color: props.color }}>Hello {props.name}</div>;
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div>Hello</div>
+      <SayHello name="Liat" color="red" />
+      <SayHello name="Lior" color="blue" />
+    </div>
+  );
 }
 
 export default App;
