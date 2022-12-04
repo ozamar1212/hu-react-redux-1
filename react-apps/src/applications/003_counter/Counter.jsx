541901b2e70ed37f65a238b043fec1062b45d4e2
bk-software <barak.software.solutions@gmail.com>
2022-12-04 18:31:43 +0200

    unworking counter

--- react-apps/src/applications/003_counter/Counter.jsx
+++ react-apps/src/applications/003_counter/Counter.jsx
@@ -1,5 +1,17 @@
 function Counter() {
-  return <div>counter</div>;
+  let counter = 0;
+
+  function increment() {
+    counter++;
+    console.log('increment', counter);
+  }
+
+  return (
+    <div>
+      <button onClick={increment}>+</button>
+      <h2>{counter}</h2>
+    </div>
+  );
 }
 
 export default Counter;
