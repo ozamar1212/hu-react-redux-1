318d74581eb07117aaf059d9afd4e9f5a0ecd819
bk-software <barak.software.solutions@gmail.com>
2022-12-07 18:43:48 +0200

    start with app layout

---
+++ react-apps/src/applications/500_App_layout/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
