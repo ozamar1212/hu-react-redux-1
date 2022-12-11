aa948a8b88599f10f6939ea5049bf37c6f95a28a
bk-software <barak.software.solutions@gmail.com>
2022-12-11 20:50:32 +0200

    add pages link

--- react-apps/src/applications/real-app/502_App_add_router/pages/AboutPage.jsx
+++ react-apps/src/applications/real-app/502_App_add_router/pages/AboutPage.jsx
@@ -1,9 +1,11 @@
 import Layout from '../layout/Layout';
+import { Link } from 'react-router-dom';
 
 function AboutPage() {
   return (
     <Layout>
       <div>About My app</div>
+      <Link to="/">Goto Homepage</Link>
     </Layout>
   );
 }
--- react-apps/src/applications/real-app/502_App_add_router/pages/HomePage.jsx
+++ react-apps/src/applications/real-app/502_App_add_router/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Layout from '../layout/Layout';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <Layout>
       <div>Homepage</div>
+      <Link to="/about">About Page</Link>
     </Layout>
   );
 }
